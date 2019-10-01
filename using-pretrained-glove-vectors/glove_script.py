2e4493efb539c007327a5c493b7a28627f15c76c
Sebastian Keane Theiler <sebastiankt9@gmail.com>
2019-10-01 14:21:16 -0400

    Add files via upload

---
+++ using-pretrained-glove-vectors/glove_script.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy import spatial
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+embeddings_dict = {}
+with open("glove.6B.50d.txt", 'r') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], "float32")
+        embeddings_dict[word] = vector
+
+def find_closest_embeddings(embedding):
+    return sorted(embeddings_dict.keys(), key=lambda word: spatial.distance.euclidean(embeddings_dict[word], embedding))
+
+print(find_closest_embeddings(embeddings_dict["king"])[1:6])
+
+print(find_closest_embeddings(
+    embeddings_dict["twig"] - embeddings_dict["branch"] + embeddings_dict["hand"]
+)[:5])
+
+tsne = TSNE(n_components=2, random_state=0)
+words =  list(embeddings_dict.keys())
+vectors = [embeddings_dict[word] for word in words]
+Y = tsne.fit_transform(vectors[:1000])
+
+plt.scatter(Y[:, 0], Y[:, 1])
+
+for label, x, y in zip(words, Y[:, 0], Y[:, 1]):
+    plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords="offset points")
+plt.show()
